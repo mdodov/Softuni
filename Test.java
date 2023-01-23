4d524eaf2ce2e34f37c140675096542fcc08848f
mdodov <m.p.dodov@gmail.com>
2023-01-23 19:18:19 +0200

    Created class Test

---
+++ Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        int c = a + b;
+
+        System.out.println(c);
+
+    }
+
+}
